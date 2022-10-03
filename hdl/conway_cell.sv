a5c82f6ba482507863120a9b6dd63e7810203ea0
Lilo Heinrich <lheinrich@olin.edu>
2022-10-03 18:30:27 -0400

    undo my changes to conway_cell.sv

--- hdl/conway_cell.sv
+++ hdl/conway_cell.sv
@@ -13,33 +13,4 @@ output logic state_q;
 input wire [7:0] neighbors;
 
 
-
-logic [7:0] empty;
-logic no_carry;
-//logic [7:0] output_sum; 
-logic output_sum; 
-logic output_carry;
-
-parameter N = 8;
-
-adder_1 adder_1_obj(
-						.a(0), 
-						.b(1), 
-						.c_in(0), 
-						.sum(output_sum), 
-						.c_out(output_carry)
-						);
-
-
-initial begin
-$dumpfile("test_conway_cell.fst");
-$dumpvars(0, adder_1_obj);
-
-$display("adder_1_obj.a adder_1_obj.b | output_sum output_carry");
-  #1 $display("%1b %1b %1b", adder_1_obj.a, adder_1_obj.b, output_sum, output_carry);
-$finish;    
-
-end
-
-
 endmodule
