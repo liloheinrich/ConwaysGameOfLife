1aebe6951d16c6b0d68ab50aeb5c4b1ce2c0178f
Lilo Heinrich <lheinrich@olin.edu>
2022-10-16 17:56:14 -0400

    fix conway_cell.sv to make rst, ena, state_q work

--- hdl/conway_cell.sv
+++ hdl/conway_cell.sv
@@ -5,11 +5,9 @@ module conway_cell(clk, rst, ena, state_0, state_d, state_q, neighbors);
 input wire clk;
 input wire rst;
 input wire ena;
-
 input wire state_0;
 output logic state_d;
 output logic state_q;
-
 input wire [7:0] neighbors;
 
 logic [3:0] sum;
@@ -31,13 +29,16 @@ always_comb begin
 	//$display ("two_or_three_neighbors = %1b", two_or_three_neighbors);
 	//$display ("");
 
-	state_d = (~ state_0 & three_neighbors) | (state_0 & two_or_three_neighbors);
-
-	state_q = state_d;
+	// turns out state_0 isn't the input state, but rather the original starting state. 
+	// state_q is both an input and an output; we are given the previous state_q and update it to be the current state_q
+	state_d = (~ state_q & three_neighbors) | (state_q & two_or_three_neighbors);
 end
 
-//always_ff @(posedge clk) begin
-//	state_q <= state_d;
-//end 
+always_ff @(posedge clk) begin : delay
+	// reset should have priority generally, and be able to reset the circuit whether ena = 1 or not. 
+	// but this is a design decision that is sort of up to us. whatever makes the tests pass.
+	// also we are not allowed to use if-else statements in this assignent, use ternary a?b:c instead.
+	state_q <= rst ? state_0 : (ena & state_d);
+end 
 
 endmodule
