a5637bfa0378f37111a5ea875479c109b7577d4e
emmamack <emack@olin.edu>
2022-10-19 12:37:53 -0400

    Generalized led array driver

--- hdl/led_array_driver.sv
+++ hdl/led_array_driver.sv
@@ -20,6 +20,7 @@ decoder_3_to_8 decoder_3_to_8_inst(.ena(ena), .in(x), .out(x_decoded));
 
 // You can check parameters with the $error macro within initial blocks.
 initial begin
+
   if ((N <= 0) || (N > 8)) begin
     $error("N must be within 0 and 8.");
   end
@@ -31,30 +32,38 @@ initial begin
   end
 end
 
-logic row0;
-logic row1;
-logic row2;
-logic row3;
-logic row4;
-logic row5;
-logic row6;
-logic row7;
+// logic row0;
+// logic row1;
+// logic row2;
+// logic row3;
+// logic row4;
+// logic row5;
+// logic row6;
+// logic row7;
 
-always_comb begin
-  rows = {row7, row6, row5, row4, row3, row2, row1, row0};
-  cols = x_decoded;
-end
+// always_comb begin
+  // rows = {row7, row6, row5, row4, row3, row2, row1, row0};
+  // cols = x_decoded;
 
+  // row0 = ~(|(x_decoded[N-1:0] & cells[1*N-1:0*N]));
+  // row1 = ~(|(x_decoded[N-1:0] & cells[2*N-1:1*N]));
+  // row2 = ~(|(x_decoded[N-1:0] & cells[3*N-1:2*N]));
+  // row3 = ~(|(x_decoded[N-1:0] & cells[4*N-1:3*N]));
+  // row4 = ~(|(x_decoded[N-1:0] & cells[5*N-1:4*N]));
+  // row5 = ~(|(x_decoded[N-1:0] & cells[6*N-1:5*N]));
+  // row6 = ~(|(x_decoded[N-1:0] & cells[7*N-1:6*N]));
+  // row7 = ~(|(x_decoded[N-1:0] & cells[8*N-1:7*N]));
 
-always_comb begin
-  row0 = ~(|(x_decoded[N-1:0] & cells[1*N-1:0*N]));
-  row1 = ~(|(x_decoded[N-1:0] & cells[2*N-1:1*N]));
-  row2 = ~(|(x_decoded[N-1:0] & cells[3*N-1:2*N]));
-  row3 = ~(|(x_decoded[N-1:0] & cells[4*N-1:3*N]));
-  row4 = ~(|(x_decoded[N-1:0] & cells[5*N-1:4*N]));
-  row5 = ~(|(x_decoded[N-1:0] & cells[6*N-1:5*N]));
-  row6 = ~(|(x_decoded[N-1:0] & cells[7*N-1:6*N]));
-  row7 = ~(|(x_decoded[N-1:0] & cells[8*N-1:7*N]));
+  always_comb $display(">>>>>>>>>>>>>>>>>>>>>>>>>>>x inside: %d", x);
+
+  genvar i;
+  generate
+    for (i = 0; i < N; i = i + 1) begin
+      always_comb rows[i] = ~(|(x_decoded[N-1:0] & cells[(i+1)*N-1:i*N]));
+    end
+  endgenerate 
+
+  always_comb cols = x_decoded;
 
   //$display ("cells value = %8b", cells);
   //$display ("x value = %3b", x);
@@ -66,6 +75,6 @@ always_comb begin
   //$display ("%8b", row4);
   //$display ("rows value = %8b", rows);
   //$display ("");
-end
+// end
 
 endmodule
