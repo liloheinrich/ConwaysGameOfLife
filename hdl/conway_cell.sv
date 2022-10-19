5e1781b35853336e906fbc8d2fbd65e81e6c41a0
Lilo Heinrich <lheinrich@olin.edu>
2022-10-19 19:34:57 -0400

    comment conway_cell

--- hdl/conway_cell.sv
+++ hdl/conway_cell.sv
@@ -17,27 +17,33 @@ logic three_neighbors;
 logic two_neighbors;
 logic two_or_three_neighbors;
 
+// these equality comparators return one bit which signifies whether the sum of the neighbors is equal to 2 or to 3
 equality_comparator two_neighbors_comparator (.a(sum), .b(4'b0010), .out(two_neighbors));
 equality_comparator three_neighbors_comparator (.a(sum), .b(4'b0011), .out(three_neighbors));
 
 always_comb begin
 	two_or_three_neighbors = three_neighbors | two_neighbors;
 
-	//$display ("sum = %4b", sum);
-	//$display ("three_neighbors = %1b", three_neighbors);
-	//$display ("two_neighbors = %1b", two_neighbors);
-	//$display ("two_or_three_neighbors = %1b", two_or_three_neighbors);
-	//$display ("");
+	/* state_0 isn't the input state, but rather the original starting state. 
+	   state_q is both an input and an output; we are given the previous 
+	   state_q and update it to be the current state_q
+	*/
 
-	// turns out state_0 isn't the input state, but rather the original starting state. 
-	// state_q is both an input and an output; we are given the previous state_q and update it to be the current state_q
+	/* here, state_d is being set to follow the rules of Conway's game:
+	    - if the cell is dead and has 3 neighbors, it revives. 
+	    - if the cell is alive and has 2 or 3 neighbors, it survives. 
+	    - in all other cases the cell dies.
+	*/
 	state_d = (~ state_q & three_neighbors) | (state_q & two_or_three_neighbors);
 end
 
 always_ff @(posedge clk) begin : delay
-	// reset should have priority generally, and be able to reset the circuit whether ena = 1 or not. 
-	// but this is a design decision that is sort of up to us. whatever makes the tests pass.
-	// also we are not allowed to use if-else statements in this assignent, use ternary a?b:c instead.
+	/* reset should have priority (be able to reset the circuit whether ena = 1 or not)
+	   on the positive edge of the clock, state_q is set to:
+	    - reset if rst is High
+	    - state_d if ena is High
+	    - state_q if ena is Low (maintain previous state_q)
+	*/
 	state_q <= rst ? state_0 : (ena ? state_d : state_q);
 end 
 
